464db2f6164be883c95597a4b69e89f2052a4628
Niklas Nielsen <nik@qni.dk>
2024-05-24 12:17:34 -0700

    Fetch autofeedback in news application

--- service/main.py
+++ service/main.py
@@ -1,4 +1,8 @@
-from modal import App, web_endpoint, Image
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from modal import App, Image, asgi_app
 import modal
 
 image = (
@@ -9,16 +13,59 @@ image = (
 app = App("news-summarizer", image=image)
 
 
-from log10.load import OpenAI
+from log10.load import OpenAI, log10_session
 import requests
 from bs4 import BeautifulSoup
 from newsapi import NewsApiClient
+
+import os
 
 article_cache = {}
 
 # Initialize the News API client
 newsapi = NewsApiClient(api_key="cc2f2407c4184ff8a9ac99ffc90f435b")
 
+import requests
+
+def fetch_autofeedback(id):
+    api_token = os.getenv('LOG10_TOKEN')
+    org_id = os.getenv('LOG10_ORG_ID')
+
+    url = 'https://graphql.log10.io/graphql'
+    headers = {
+        'content-type': 'application/json',
+        'x-api-token': api_token
+    }
+    query = """
+    query OrganizationCompletion($orgId: String!, $id: String!) {
+        organization(id: $orgId) {
+            completion(id: $id) {
+                id
+                autoFeedback {
+                    id
+                    status
+                    jsonValues
+                    comment
+                }
+            }
+        }
+    }
+    """
+    variables = {
+        'orgId': org_id,
+        'id': id
+    }
+    payload = {
+        'query': query,
+        'variables': variables
+    }
+    
+    response = requests.post(url, headers=headers, json=payload)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
 
 def scrape_article(url):
     response = requests.get(url)
@@ -43,21 +90,29 @@ def summarize_article(content, autofeedback):
     tags = ["news"]
     if autofeedback:
         tags.append("log10/summary-grading")
-    client = OpenAI(tags=tags)
+    client = OpenAI()
 
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": "You are an expert summarizer."},
-            {
-                "role": "user",
-                "content": "Summarize the following article into a tweet sized summary:",
-            },
-            {"role": "user", "content": content},
-        ],
-    )
+    with log10_session(tags=tags) as session:
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": "You are an expert summarizer."},
+                {
+                    "role": "user",
+                    "content": "Summarize the following article into a tweet sized summary:",
+                },
+                {"role": "user", "content": content},
+            ],
+        )
 
-    return response.choices[0].message.content
+        url = session.last_completion_url()
+        completion_id = None
+        if url:
+            completion_id = url.split("/")[-1]
+
+    # Get the completion id.
+
+    return completion_id, response.choices[0].message.content
 
 
 # # @app.route('/news', methods=['GET'])
@@ -77,9 +132,31 @@ def summarize_article(content, autofeedback):
 # #     return jsonify(news_with_summaries)
 
 
-@app.function(secrets=[modal.Secret.from_name("news-secrets")])
-@web_endpoint()
-def news(reset_cache: bool = False, autofeedback: bool = False, start=0, end=10):
+web_app = FastAPI()
+web_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can specify specific origins instead of ["*"]
+    allow_credentials=True,
+    allow_methods=["*"],  # You can specify specific methods instead of ["*"]
+    allow_headers=["*"],  # You can specify specific headers instead of ["*"]
+)
+
+@app.function(secrets=[modal.Secret.from_name("news-secrets")], image=image)
+@asgi_app()
+def fastapi_app():
+    return web_app
+
+
+@web_app.get("/autofeedback")
+async def autofeedback(completion_id=None):
+    if not completion_id:
+        return {"error": "Please provide a completion ID."}
+    
+    return fetch_autofeedback(completion_id)
+
+
+@web_app.get("/news")
+async def news(reset_cache: bool = False, autofeedback: bool = False, start=0, end=10):
     if reset_cache:
         article_cache.clear()
 
@@ -92,13 +169,15 @@ def news(reset_cache: bool = False, autofeedback: bool = False, start=0, end=10)
         if article["url"] in article_cache:
             summary = article_cache[article["url"]].get("summary")
             content = article_cache[article["url"]].get("content")
+            completion_id = article_cache[article["url"]].get("completion_id")
         else:
             content = scrape_article(article["url"])
             if content:
-                summary = summarize_article(content, autofeedback)
+                completion_id, summary = summarize_article(content, autofeedback)
                 article_cache[article["url"]] = {
                     "summary": summary,
                     "content": content,
+                    "completion_id": completion_id,
                 }
 
         news_with_summaries.append(
@@ -108,6 +187,7 @@ def news(reset_cache: bool = False, autofeedback: bool = False, start=0, end=10)
                 "title": article["title"],
                 "url": article["url"],
                 "image": article["urlToImage"],
+                "completion_id": completion_id,
             }
         )
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,219 @@ import { RiRestartFill, RiRestartLine } from "@remixicon/react";
 
 // TODO: Make it easy to report summary quality.
 
+function classNames(...classes: any[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+function Comment({ feedback }: any) {
+  return (
+    <form action="#" className="relative">
+      <div className="mt-4 w-full overflow-hidden rounded-lg border border-gray-300 shadow-sm focus-within:border-indigo-500 focus-within:ring-1 focus-within:ring-indigo-500">
+        <p className="pt-2 pl-3 text-xs text-gray-400 font-bold">
+          AutoFeedback
+        </p>
+        <label htmlFor="description" className="sr-only">
+          Description
+        </label>
+        <textarea
+          rows={2}
+          name="description"
+          id="description"
+          className="block w-full resize-none border-0 py-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+          placeholder="Write a description..."
+          defaultValue={feedback?.comment}
+        />
+
+        {/* Spacer element to match the height of the toolbar */}
+        <div aria-hidden="true">
+          <div className="py-2">
+            <div className="h-9" />
+          </div>
+          <div className="h-px" />
+          <div className="py-2">
+            <div className="py-px">
+              <div className="h-9" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="absolute inset-x-px bottom-0">
+        {/* Actions: These are just examples to demonstrate the concept, replace/wire these up however makes sense for your project. */}
+        <div className="flex flex-nowrap justify-end space-x-2 px-2 py-2 sm:px-3">
+          <div className="flex-shrink-0">
+            <div className="relative">
+              <div className="relative inline-flex items-center whitespace-nowrap rounded-full bg-gray-50 px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 sm:px-3">
+                <span
+                  className={classNames("hidden truncate sm:ml-2 sm:block")}
+                >
+                  {Math.floor(
+                    ((feedback?.jsonValues?.Accuracy || 0) / 7) * 100
+                  )}{" "}
+                  % accuracy
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex-shrink-0">
+            <div className="relative">
+              <div className="relative inline-flex items-center whitespace-nowrap rounded-full bg-gray-50 px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 sm:px-3">
+                <span
+                  className={classNames("hidden truncate sm:ml-2 sm:block")}
+                >
+                  {Math.floor(
+                    ((feedback?.jsonValues?.Coherence || 0) / 7) * 100
+                  )}{" "}
+                  % coherence
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex-shrink-0">
+            <div className="relative">
+              <div className="relative inline-flex items-center whitespace-nowrap rounded-full bg-gray-50 px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 sm:px-3">
+                <span
+                  className={classNames("hidden truncate sm:ml-2 sm:block")}
+                >
+                  {Math.floor(
+                    ((feedback?.jsonValues?.Coverage || 0) / 7) * 100
+                  )}{" "}
+                  % coverage
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex-shrink-0">
+            <div className="relative">
+              <div className="relative inline-flex items-center whitespace-nowrap rounded-full bg-gray-50 px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 sm:px-3">
+                <span
+                  className={classNames("hidden truncate sm:ml-2 sm:block")}
+                >
+                  {Math.floor(((feedback?.jsonValues?.Overall || 0) / 7) * 100)}{" "}
+                  % overall
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+const Post = ({ post, autofeedback }: any) => {
+  // Poll for autofeedback at
+  // https://log10-io--news-summarizer-fastapi-app-dev.modal.run/autofeedback?completion_id=completion_id
+
+  const [feedback, setFeedback] = useState<any>();
+
+  // If not found, try again in 1 second.
+  useEffect(() => {
+    if (post.completion_id) {
+      const interval = setInterval(() => {
+        fetch(
+          `https://log10-io--news-summarizer-fastapi-app.modal.run/autofeedback?completion_id=${post.completion_id}`
+        )
+          .then((response) => response.json())
+          .then((data) => {
+            if (data) {
+              const f = data.data.organization?.completion?.autoFeedback;
+              console.log("Feedback", f, "data", data);
+
+              if (f) {
+                setFeedback(f);
+                console.log("Feedback found, clearing interval");
+                clearInterval(interval);
+              }
+            }
+          });
+      }, 1000);
+    }
+  }, [post.completion_id]);
+
+  return (
+    <article
+      key={post.url}
+      className="relative isolate flex flex-col gap-8 lg:flex-row"
+    >
+      <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
+        <img
+          src={post.image}
+          alt=""
+          className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
+        />
+        <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
+      </div>
+      <div>
+        <div className="flex items-center gap-x-4 text-xs">
+          {/* <time dateTime={post.datetime} className="text-gray-500">
+      {post.date}
+    </time> */}
+          {/* <a
+      href={post.category.href}
+      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+    >
+      {post.category.title}
+    </a> */}
+        </div>
+        <div className="group relative">
+          <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+            <a href={post.url}>
+              <span className="absolute inset-0" />
+              {post.title}
+            </a>
+          </h3>
+          <p className="mt-5 text-sm leading-6 text-gray-600">{post.summary}</p>
+          {feedback && <Comment feedback={feedback} />}
+          {(autofeedback && !feedback) && (
+            <div
+              role="status"
+              className="flex flex-col justify-center items-center mt-4"
+            >
+              <svg
+                aria-hidden="true"
+                className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentFill"
+                />
+              </svg>
+              <Text>Autofeedback generating</Text>
+              <span className="sr-only">Loading...</span>
+            </div>
+          )}
+        </div>
+        {/* <div className="mt-6 flex border-t border-gray-900/5 pt-6">
+    <div className="relative flex items-center gap-x-4">
+      <img
+        src={post.author.imageUrl}
+        alt=""
+        className="h-10 w-10 rounded-full bg-gray-50"
+      />
+      <div className="text-sm leading-6">
+        <p className="font-semibold text-gray-900">
+          <a href={post.author.href}>
+            <span className="absolute inset-0" />
+            {post.author.name}
+          </a>
+        </p>
+        <p className="text-gray-600">{post.author.role}</p>
+      </div>
+    </div>
+  </div> */}
+      </div>
+    </article>
+  );
+};
+
 export default function Example() {
   const [posts, setPosts] = useState<any>([]);
   const [autofeedback, setAutofeedback] = useState(false);
@@ -16,7 +229,7 @@ export default function Example() {
 
       for (let i = 0; i < 10; i++) {
         fetch(
-          `https://log10-io--news-summarizer-news.modal.run?autofeedback=${autofeedback}&reset_cache=${reset}&start=${i}&end=${
+          `https://log10-io--news-summarizer-fastapi-app.modal.run/news?autofeedback=${autofeedback}&reset_cache=${reset}&start=${i}&end=${
             i + 1
           }`
         )
@@ -106,61 +319,7 @@ export default function Example() {
           {
             <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
               {posts.map((post: any) => (
-                <article
-                  key={post.url}
-                  className="relative isolate flex flex-col gap-8 lg:flex-row"
-                >
-                  <div className="relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0">
-                    <img
-                      src={post.image}
-                      alt=""
-                      className="absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover"
-                    />
-                    <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-                  </div>
-                  <div>
-                    <div className="flex items-center gap-x-4 text-xs">
-                      {/* <time dateTime={post.datetime} className="text-gray-500">
-                      {post.date}
-                    </time> */}
-                      {/* <a
-                      href={post.category.href}
-                      className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                    >
-                      {post.category.title}
-                    </a> */}
-                    </div>
-                    <div className="group relative max-w-xl">
-                      <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                        <a href={post.url}>
-                          <span className="absolute inset-0" />
-                          {post.title}
-                        </a>
-                      </h3>
-                      <p className="mt-5 text-sm leading-6 text-gray-600">
-                        {post.summary}
-                      </p>
-                    </div>
-                    {/* <div className="mt-6 flex border-t border-gray-900/5 pt-6">
-                    <div className="relative flex items-center gap-x-4">
-                      <img
-                        src={post.author.imageUrl}
-                        alt=""
-                        className="h-10 w-10 rounded-full bg-gray-50"
-                      />
-                      <div className="text-sm leading-6">
-                        <p className="font-semibold text-gray-900">
-                          <a href={post.author.href}>
-                            <span className="absolute inset-0" />
-                            {post.author.name}
-                          </a>
-                        </p>
-                        <p className="text-gray-600">{post.author.role}</p>
-                      </div>
-                    </div>
-                  </div> */}
-                  </div>
-                </article>
+                <Post key={post.url} post={post} autofeedback={autofeedback} />
               ))}
             </div>
           }
